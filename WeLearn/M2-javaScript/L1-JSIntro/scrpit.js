fe902472bdb5cc329b116ba6ee07ba36199760c2
Daniel Marin <marin082@csusm.edu>
2019-07-24 12:00:27 -0700

    java Script intro

---
+++ WeLearn/M2-javaScript/L1-JSIntro/scrpit.js
@@ -0,0 +1,26 @@
+console.log("script running!");
+
+let name = 'Charlie';
+console.log('Hello ' + name + '!');
+let age = 15;
+console.log('You are ' + age + ' years old!');
+
+let age = 16;
+if (age >= 16){
+
+} else {
+
+}
+if (age < 18 && name === "John") {
+  ...
+}
+if (age < 18 || age > 80){
+  ...
+}
+let raining = false;
+if (!raining){
+  console.log('Today is a sunny day');
+}
+if (animal === 'dog' || animal === ''cat'' || animal === 'cow')){
+  ...
+}
