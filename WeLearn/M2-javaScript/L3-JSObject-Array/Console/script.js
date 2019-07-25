c5963cebad0688fa46f2942dffa8a5f1c21dec16
Daniel Marin <marin082@csusm.edu>
2019-07-25 12:08:10 -0700

    L4 loops

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,36 @@
+const article = {
+  name: 'Dog family gives birth to 10 puppies',
+  veiws: 1035,
+  datePublished: '3/25/2018',
+  author: {
+    name: 'Joe Corgi',
+    title: 'Senior Canine Editor'
+  }
+};
+
+const favoriteFood = {
+  food1: 'Pizza',
+  food2: 'CheeseBurger',
+  food3: 'Spaghetti',
+  food4: 'Chicken',
+  food5: 'eggs',
+  food6: 'quesadilla',
+  food7: 'nac-n-cheese',
+  food8: 'rice',
+  food9: 'lol',
+  food10: 'souls of the weak',
+};
+
+const seasons = ['Spring', 'Summer', 'Fall', 'Winter'];
+const friends = ['Fancy', 'Kirby', 'Pincher', 'Bell'];
+const numbers = [5, 10, 2, 16, 50];
+
+const classes = [];
+classes.push({course: 'CS 111', title: 'Computer Science I'});
+classes.push({course: 'Math160', title: 'Calculus I'});
+
+const party = {
+  date: '7/4/2018',
+  location: 'Library',
+  guests: ['Birbys', 'Doggos', 'Hoomans']
+};
