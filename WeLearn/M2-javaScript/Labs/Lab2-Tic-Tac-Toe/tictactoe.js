e56679fb64f7e14a64ae1d65e3e4bca3630b3442
Daniel Marin <marin082@csusm.edu>
2019-07-25 16:19:45 -0700

    TicTacToe Lab

---
+++ WeLearn/M2-javaScript/Labs/Lab2-Tic-Tac-Toe/tictactoe.js
@@ -0,0 +1,54 @@
+const  button1= document.querySelector('#button1');
+const  button2= document.querySelector('#button2');
+const  button3= document.querySelector('#button3');
+const  button4= document.querySelector('#button4');
+const  button5= document.querySelector('#button5');
+const  button6= document.querySelector('#button6');
+const  button7= document.querySelector('#button7');
+const  button8= document.querySelector('#button8');
+const  button9= document.querySelector('#button9');
+
+button1.addEventListener('click', (e) => {
+  button1.style.backgroundColor = "red";
+  button1.innerHTML = "X"
+});
+
+button2.addEventListener('click', (e) => {
+  button2.style.backgroundColor = "red";
+  button2.innerHTML = 'X'
+});
+
+button3.addEventListener('click', (e) => {
+  button3.style.backgroundColor = "red";
+  button3.innerHTML = 'X'
+});
+
+button4.addEventListener('click', (e) => {
+  button4.style.backgroundColor = "red";
+  button4.innerHTML = 'X'
+});
+
+button5.addEventListener('click', (e) => {
+  button5.style.backgroundColor = "red";
+  button5.innerHTML = 'X'
+});
+
+button6.addEventListener('click', (e) => {
+  button6.style.backgroundColor = "red";
+  button6.innerHTML = 'X'
+});
+
+button7.addEventListener('click', (e) => {
+  button7.style.backgroundColor = "red";
+  button7.innerHTML = 'X'
+});
+
+button8.addEventListener('click', (e) => {
+  button8.style.backgroundColor = "red";
+  button8.innerHTML = 'X'
+});
+
+button9.addEventListener('click', (e) => {
+  button9.style.backgroundColor = "red";
+  button9.innerHTML = 'X'
+});
