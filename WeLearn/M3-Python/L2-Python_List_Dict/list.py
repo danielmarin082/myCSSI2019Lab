1a13f8f25dd80ae1ea484118ec51e46937ee1253
Daniel Marin <marin082@csusm.edu>
2019-07-31 11:55:54 -0700

    python lists practice

---
+++ WeLearn/M3-Python/L2-Python_List_Dict/list.py
@@ -0,0 +1,31 @@
+# students = ["Alice", "Javi", "Damien", "Javi"]
+# students.remove("Javi")
+# print(students)
+
+#smith_siblings = ["Emily", "Monique", "Giovanni", "Thanos", "Bubby", "Pyrocynical", "Smith"]
+# for name in smith_siblings:
+#     print(name + " Smith")
+
+# count = 0
+# for name in smith_siblings:
+#     count = count + 1
+# print (count)
+
+# print(len(smith_siblings))
+
+# for index in range(len(smith_siblings)):
+#     smith_siblings[index] = smith_siblings[index] + " Smith"
+#
+# print(smith_siblings)
+
+# superheroes = ["Captain Karen", "Spiderman", "Spiderman", "Spiderman", "Spiderman", "Spiderman"]
+# demoted_hero = str(raw_input("What superhero do you want to eliminate from the top 5?"))
+# if demoted_hero in superheroes:
+#     superheroes.remove(demoted_hero)
+#     print("Top 5 heroes", superheroes)
+# else:
+#     print("Hero not found.")
+
+names = ["Rickon", "Bran", "Arya", "Sans", "Jon", "Robb"]
+# names[4:2:-1]
+print(names[::2])
